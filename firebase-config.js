f42d020f50f0d9e3108d883549e8601d30809572
CamilleJCC <122910843+CamilleJCC@users.noreply.github.com>
2025-01-21 16:33:52 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyBYISQS9bHDbPSz_WzGx7sG1gI_C-GfAzM",
   authDomain: "mapfre-voltea.firebaseapp.com",
   databaseURL: "https://mapfre-voltea-default-rtdb.firebaseio.com",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: "1:532381967364:web:5671da618c94acd265c151",
   measurementId: "G-3R574NY98V"
 };
-
+*/
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
